f6ead52de715c73ec88a957456f923d647fa5181
kajal-2011 <kajalrekha888@gmail.com>
2023-02-14 01:29:27 +0600

    routes added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+
 const express = require("express");
+const projectRoutes = require("./routes/projectRoutes");
 
 // express app
 const app = express();
@@ -7,7 +9,17 @@ const app = express();
 // port
 const port = process.env.PORT || 4000;
 
+// middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//routes
+app.use("/api/projects", projectRoutes);
+
 // listen for request
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log(`listen on port ${port}`);
 });
