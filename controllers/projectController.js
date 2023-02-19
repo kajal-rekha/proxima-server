2e36a6171cf92aa6f87fd5275b47e19dcc044f45
kajal-2011 <kajalrekha888@gmail.com>
2023-02-20 01:53:53 +0600

    patch empty fields added

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -18,23 +18,61 @@ const getSingleProject = async (req, res) => {
   }
 
   const project = await Project.findById(id);
+
   if (!project) {
     return res.status(404).json({ error: "No project found" });
   }
+
   res.status(200).json(project);
 };
+
 // post a new project
 const postProject = async (req, res) => {
-  const data = req.body;
+  const { title, tech, budget, duration, manager, dev } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+
+  if (!tech) {
+    emptyFields.push("tech");
+  }
+
+  if (!budget) {
+    emptyFields.push("budget");
+  }
+
+  if (!duration) {
+    emptyFields.push("duration");
+  }
+
+  if (!manager) {
+    emptyFields.push("manager");
+  }
+
+  if (!dev) {
+    emptyFields.push("dev");
+  }
+
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all fields", emptyFields });
+  }
+
   try {
     const project = await Project.create({
-      ...data,
+      ...req.body,
     });
+
     res.status(200).json(project);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
+
 // delete a project
 const deleteProject = async (req, res) => {
   const { id } = req.params;
@@ -54,9 +92,44 @@ const deleteProject = async (req, res) => {
 const updateProject = async (req, res) => {
   const { id } = req.params;
 
+  const { title, tech, budget, duration, manager, dev } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+
+  if (!tech) {
+    emptyFields.push("tech");
+  }
+
+  if (!budget) {
+    emptyFields.push("budget");
+  }
+
+  if (!duration) {
+    emptyFields.push("duration");
+  }
+
+  if (!manager) {
+    emptyFields.push("manager");
+  }
+
+  if (!dev) {
+    emptyFields.push("dev");
+  }
+
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all fields", emptyFields });
+  }
+
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "Invalid id" });
   }
+
   const project = await Project.findOneAndUpdate({ _id: id }, { ...req.body });
 
   if (!project) {
