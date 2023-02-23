3d76fb7b1d7182d560167820df0faacc78258089
kajal-2011 <kajalrekha888@gmail.com>
2023-02-23 14:46:08 +0600

    bcrypt added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,20 @@
 const User = require("../models/userModal");
 
-//loginUser
+//login User
 const loginUser = async (req, res) => {
   res.json({ message: "login" });
 };
 
-// signuUser
+// signup User
 const signupUser = async (req, res) => {
-  res.json({ message: "signup" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 module.exports = {
